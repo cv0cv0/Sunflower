b50bc37d4edb69657182b391ad4217ecf14115ee
cv0cv0 <linux2.0@foxmail.com>
2018-09-22 17:14:29 +0800

    Initial commit

---
+++ app/src/main/java/me/gr/sunflower/ui/GardenFragment.kt
@@ -0,0 +1,13 @@
+package me.gr.sunflower.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class GardenFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
